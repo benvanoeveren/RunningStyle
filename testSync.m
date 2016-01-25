a936ba98f3a6ce67b6a5c1ddbc406a302b37a27e
benvanoeveren <benvoeveren@gmail.com>
2016-01-25 17:27:41 +0100

    asdfa

--- testSync.m
+++ testSync.m
@@ -1,8 +1,15 @@
 % testSync
 
+<<<<<<< Updated upstream
 %% misschien nog dit en dat
 x=1:10;
+<<<<<<< Updated upstream
 
 
 
 
+=======
+=======
+%% misschien nog dit en dat
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
